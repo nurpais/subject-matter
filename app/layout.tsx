dc641f29d12f077cfe6ebc3fdb79282ebd1d81ad
Nurpais <orozulbaev@gmail.com>
2024-01-18 22:11:45 +0600

    Fix empty page h-screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,11 +12,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="h-full">
+      <body className="flex h-full flex-col">
         <NavbarContextProvider>
           <Navbar />
-          {children}
+          <div className="flex-auto">{children}</div>
           <Footer />
         </NavbarContextProvider>
       </body>
